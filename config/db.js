84ae6a2e6a7752fd0b01df3e03f81e596bdd0ba4
Rivelinoortiz507 <rivelinoortiz123@gmail.com>
2025-02-12 13:40:55 -0500

    Actualizacion para funcionar despliegue

--- config/db.js
+++ config/db.js
@@ -6,6 +6,18 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectTimeout: 10000,  // Ajusta el tiempo de espera si es necesario
 });
+
+// Verificar la conexión
+pool.getConnection()
+  .then(connection => {
+    console.log('✅ Conectado a la base de datos MySQL en Railway');
+    connection.release(); // Liberar conexión del pool
+  })
+  .catch(error => {
+    console.error('❌ Error al conectar a MySQL:', error.message);
+  });
 
 module.exports = pool;
